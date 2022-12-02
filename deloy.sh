769a4c083f314bad83aa8039f389a83d3ad6f190
davii16 <davigithub16@gmail.com>
2022-12-02 08:15:13 +0700

    update .env.example, change 404 page , add deloy sh

---
+++ deloy.sh
@@ -0,0 +1,34 @@
+# Turn on maintenance mode
+php artisan down
+
+# Pull the latest changes from the git repository
+git pull -f origin master
+# update pr
+sudo chown -R www-data:www-data .
+# Install/update composer dependecies
+composer install --no-interaction --prefer-dist --optimize-autoloader
+# Install node modules
+npm install --no-audit
+# Run database migrations
+sudo chown -R www-data:www-data .
+#
+php artisan migrate --force
+
+# Clear caches
+php artisan cache:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache config
+php artisan config:clear
+php artisan config:cache
+# init server
+sudo laravel-echo-server init
+#
+sudo chown -R www-data:www-data .
+#
+sudo supervisorctl restart all
+
+# Turn off maintenance mode
+php artisan up
--- resources/js/pages/NotFound.vue
+++ resources/js/pages/NotFound.vue
@@ -1,118 +1,59 @@
 <template>
-  <div class="stage">
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-  </div>
+    <div id="main">
+        <div class="fof">
+            <h1>Error 404</h1>
+            <v-btn
+                rounded
+                color="primary"
+                :to="{ name: 'home' }"
+                class="ml-4"
+                dark
+            >
+                QUAY VỀ TRANG CHỦ
+            </v-btn>
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
-@import url("//fonts.googleapis.com/css?family=Pacifico&text=Pure");
-@import url("//fonts.googleapis.com/css?family=Roboto:700&text=css");
-@import url("//fonts.googleapis.com/css?family=Kaushan+Script&text=!");
+* {
+    transition: all 0.6s;
+}
+
+html {
+    height: 100%;
+}
 
 body {
-  min-height: 450px;
-  height: 100vh;
-  margin: 0;
-  background: radial-gradient(
-    circle,
-    darken(dodgerblue, 10%),
-    #1f4f96,
-    #1b2949,
-    #000
-  );
+    font-family: "Lato", sans-serif;
+    color: #888;
+    margin: 0;
 }
 
-.stage {
-  height: 300px;
-  width: 500px;
-  margin: auto;
-  position: absolute;
-  top: 400px;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  perspective: 9999px;
-  transform-style: preserve-3d;
-
+#main {
+    display: table;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
 }
 
-.layer {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  transform-style: preserve-3d;
-  animation: ಠ_ಠ 5s infinite alternate ease-in-out -7.5s;
-  animation-fill-mode: forwards;
-  transform: rotateY(40deg) rotateX(33deg) translateZ(0);
+.fof {
+    display: table-cell;
+    vertical-align: middle;
 }
 
-.layer:after {
-  font: 150px/0.65 "Pacifico", "Kaushan Script", Futura, "Roboto",
-    "Trebuchet MS", Helvetica, sans-serif;
-  content: "404  :( \A   Page not\A Found";
-  white-space: pre;
-  text-align: center;
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 50px;
-  color: darken(#fff, 4%);
-  letter-spacing: -2px;
-  text-shadow: 4px 0 10px hsla(0, 0%, 0%, 0.13);
+.fof h1 {
+    font-size: 50px;
+    display: inline-block;
+    padding-right: 12px;
+    animation: type 0.5s alternate infinite;
 }
 
-$i: 1;
-$NUM_LAYERS: 20;
-@for $i from 1 through $NUM_LAYERS {
-  .layer:nth-child(#{$i}):after {
-    transform: translateZ(($i - 1) * -1.5px);
-  }
-}
-
-.layer:nth-child(n + #{round($NUM_LAYERS/2)}):after {
-  -webkit-text-stroke: 3px hsla(0, 0%, 0%, 0.25);
-}
-
-.layer:nth-child(n + #{round($NUM_LAYERS/2 + 1)}):after {
-  -webkit-text-stroke: 15px dodgerblue;
-  text-shadow: 6px 0 6px darken(dodgerblue, 35%),
-    5px 5px 5px darken(dodgerblue, 40%), 0 6px 6px darken(dodgerblue, 35%);
-}
-
-.layer:nth-child(n + #{round($NUM_LAYERS/2 + 2)}):after {
-  -webkit-text-stroke: 15px darken(dodgerblue, 10%);
-}
-
-.layer:last-child:after {
-  -webkit-text-stroke: 17px hsla(0, 0%, 0%, 0.1);
-}
-
-.layer:first-child:after {
-  color: #fff;
-  text-shadow: none;
-}
-
-@keyframes ಠ_ಠ {
-  100% {
-    transform: rotateY(-40deg) rotateX(-43deg);
-  }
+@keyframes type {
+    from {
+        box-shadow: inset -3px 0px 0px #888;
+    }
+    to {
+        box-shadow: inset -3px 0px 0px transparent;
+    }
 }
 </style>
